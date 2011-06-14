e982cd8520dd71ca2297aa52df7be2381a730733
Andrius Bentkus <andrius.bentkus@gmail.com>
2011-06-14 18:43:39 +0200

    Add response checking functionality.
    Need to objective that actually.

--- WebPayment.php
+++ WebPayment.php
@@ -3,16 +3,219 @@
 abstract class WebPayment
 {
   /**
+    * WebToPay protocol version.
+    */
+  const VERSION = '1.4';
+
+  /**
+    * Server URL where all requests should go.
+    */
+  const PAY_URL = 'https://www.mokejimai.lt/pay/';
+
+  /**
+    * Server URL where we can get the XML with payment method data.
+    */
+  const XML_URL = 'https://www.mokejimai.lt/new/lt/lib_web_to_pays/api/';
+
+  /**
+    * SMS answer url.
+    */
+  const SMS_ANSWER_URL = 'https://www.mokejimai.lt/psms/respond/';
+
+  /**
     * Prefix for callback data.
     */
   const PREFIX = 'wp_';
 
+  /**
+   * If true, check SS2 if false, skip to SS1
+   */
+  private static $SS2 = true;
+
+  /**
+    * Toggle SS2 checking. Usualy you don't need to use this method, because
+    * by default first SS2 support are checked and if it doesn't work,
+    * it falls back to to SS1.
+    *
+    * Use this method if your server supports SS2, but you want to use SS1.
+    */
+  public static function toggleSS2($value) {
+    self::$SS2 = (bool)$value;
+  }
+
+  /**
+    * Check if SS2 checking is available and enabled.
+    *
+    * @return bool
+    */
+  public static function useSS2() {
+    return self::$SS2 && function_exists('openssl_pkey_get_public');
+  }
+
+  /**
+    * Check for SS1, which is not depend on openssl functions.
+    *
+    * @param  array  $response
+    * @param  string $passwd
+    * @param  int    $orderid
+    * @return bool
+    */
+  public static function checkSS1($response, $passwd, $orderid) {
+    if (strlen($passwd) != 32) {
+      $passwd = md5($passwd);
+    }
+
+    $_SS1 = array(
+      $passwd,
+      $orderid,
+      intval($response['test']),
+      1
+    );
+
+    $_SS1 = implode('|', $_SS1);
+
+    return $response['_ss1'] == md5($_SS1);
+  }
+
+  private static function getUrlContent($URL) {
+
+    $url = parse_url($URL);
+
+    if ($url['scheme'] == 'https') {
+      $host = 'ssl://'.$url['host'];
+      $port = 443;
+    } else {
+      $host = $url['host'];
+      $port = 80;
+    }
+
+    try {
+      $fp = fsockopen($host, $port, $errno, $errstr, 30);
+      if (!$fp) {
+        throw new Exception('Can\'t connect to %s');
+      }
+
+      if (isset($url['query'])) {
+        $data = $url['path'] . '?' . $url['query'];
+      } else {
+        $data = $url['path'];
+      }
+
+      $out = "GET " . $data . " HTTP/1.1\r\n";
+      $out .= "Host: " . $url['host'] . "\r\n";
+      $out .= "Connection: Close\r\n\r\n";
+
+      $content = '';
+
+      fwrite($fp, $out);
+      while (!feof($fp)) {
+        $content .= fgets($fp, 8192);
+      }
+      fclose($fp);
+
+      list($header, $content) = explode("\r\n\r\n", $content, 2);
+
+      return trim($content);
+
+    } catch (Exception $e) {
+      throw new Exception('fsockopen fail!');
+    }
+  }
+
+  /**
+    * A variable containing the certificate used for SS2 checking.
+    */
+  private static $certificate = null;
+
+  public static function setCertificate($certificate) {
+    $this->certificate = $certificate;
+  }
+
+  public static function getCertificate() {
+    return $this->certificate;
+  }
+
+  /**
+    * Download certificate from webtopay.com.
+    *
+    * @param  string $cert
+    * @return string
+    */
+  public static function setCertificateFromWeb($web = 'http://downloads.webtopay.com/download/', $cert = 'public.key') {
+    self::setCertificate(self::getUrlContent($web . $cert));
+    return self::getCertificate() != null;
+  }
+
+  /**
+    * Load certificate from disk.
+    *
+    * @param  string path
+    @ @return bool
+    */
+  public static function setCertificateFromDisk($path) {
+    // TODO: implement
+  }
+
+  /**
+    * Check if the saved certificate is valid.
+    *
+    * @return bool
+    */
+  public static function validCertificate() {
+    self::getCertificate != null;
+  }
+
+  /**
+    * Check if the response certificate is valid.
+    */
+  public static function checkSS2($response) {
+    $_SS2 = '';
+
+    foreach ($response as $key => $value) {
+      if ($key != '_ss2') $_SS2 .= "{$value}|";
+    }
+
+    $ok = openssl_verify($_SS2, base64_decode($response['_ss2']), self::getCertificate());
+
+    return $ok === 1;
+  }
+
+  public static function checkResponse($response, $user_data) {
+
+    $orderid  = $response['id'];
+    $password = $user_data['sign_password'];
+
+    if (self::useSS2()) {
+      if (!self::validCertificate()) {
+        self::setCertificateFromWeb();
+      }
+        
+      // If the certificate is still invalid, raise an exception
+      if (!self::validCertificate()) {
+        throw new Exception("Failed to load certificate");
+      }
+
+      // Verify the data.
+      if (self::checkSS2($response)) {
+        // Hooray, everything is a
+      }
+    } else if (self::checkSS1($response)) {
+      // at least our back up method works!
+    } else {
+      // All attempts to verify the data failed.
+    }
+  }
+
+  /**
+    * Array of ReflectionClasses for the PrefixMicroWebPayment
+    * derived classes.
+    */
   protected static $prefixes = array();
 
   /**
     * Register a class derived from PrefixMicroWebPayment
     * in order to automatically create the appropriate object
-    * with create if the prefix matches
+    * with create if the prefix matches.
     *
     * @param className the class name of the class you want to register
     * @return          boolean, true for successful registering, false for non
@@ -21,8 +224,6 @@ abstract class WebPayment
     if (!is_subclass_of($className, 'PrefixMicroWebPayment')) {
       return false;
     }
-    //array_push(self::$prefixes, $className);
-    //self::$prefixes[$className] = new ReflectionClass($className);
     array_push(self::$prefixes, new ReflectionClass($className));
     return true;
   }
@@ -51,15 +252,6 @@ abstract class WebPayment
     return $ret;
   }
 
-  protected $test;
-  public function isTest() {
-    return $this->test;
-  }
-  
-  public function __construct($response) {
-    $this->test = isset($response['test']);
-  }
-
   /**
     * Factory method for creating the appropriate
     * class objects for different responses.
@@ -72,9 +264,8 @@ abstract class WebPayment
     * @return         An instance of the appropriate object.
     *
     */
-  public function create($response) {
+  public static function create($response) {
     $response = self::getPrefixed($response);
-
 
     if (MicroWebPayment::equals($response)) {
       // first check registered prefixed microwebpayments
@@ -82,6 +273,8 @@ abstract class WebPayment
         // This doesn't work for some reason on abstract methods.
         //$method = $class->getMethod('checkValidKey');
         //if ($method && $method->invoke(null, $response['key'])) {
+
+        // We use this method, which is even documented in the php bug tracker
         if (call_user_func(array($class->getName(), 'checkValidKey'), $response['key'])) {
             return $class->newInstance($response);
         }
@@ -114,6 +307,15 @@ abstract class WebPayment
     * @return array of fields and constraints.
     */
   abstract static function getResponseSpec();
+
+  protected $test;
+  public function isTest() {
+    return $this->test;
+  }
+  
+  public function __construct($response) {
+    $this->test = isset($response['test']);
+  }
 }
 
 class MicroWebPayment extends WebPayment
